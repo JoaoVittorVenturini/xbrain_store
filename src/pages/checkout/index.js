0f0b2f91645d837287f1d87daf1ece767b5b8bfe
João Vittor Venturini <joaovittorventurini@outlook.com>
2022-12-19 17:53:29 -0300

    v1.4

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -3,13 +3,13 @@ import Purchase from 'assets/purchase.png';
 import { Link } from 'react-router-dom';
 import React from 'react';
 
-function Checkout() {
+function Checkout(props) {
   return (
     <section className='checkout'>
       <div className='checkout__container'>
         <p className='checkout__message'>
-          <strong className='checkout__message--clientName'>{},</strong>
-          sua compra no valor de <strong className='checkout__message--price'>R$ 100,00</strong> foi realizada com sucesso
+          <strong className='checkout__message--clientName'>{props.nome},</strong>
+          sua compra no valor de <strong className='checkout__message--price'>R$ {props.total}</strong> foi realizada com sucesso
         </p>
         <img className='checkout__image' src={Purchase} alt='Compra realizada com sucesso' />
         <Link className='checkout__button' to='/'>
